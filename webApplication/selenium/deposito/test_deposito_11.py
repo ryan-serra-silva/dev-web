29d37253acb4ba49ca5f589c6db987fff11e7f20
Ryan Serra <ryanserrasilva@id.uff.br>
2025-10-26 17:40:14 -0300

    Criação dos testes de deposito selenium

---
+++ webApplication/selenium/deposito/test_deposito_11.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+def main():
+    chrome_options = Options()
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--disable-notifications")
+    chrome_options.add_argument("--disable-infobars")
+
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+
+    try:
+        driver.get("http://localhost:8080/Login")
+        email_input = WebDriverWait(driver, 15).until(EC.visibility_of_element_located((By.ID, "email")))
+        senha_input = WebDriverWait(driver, 15).until(EC.visibility_of_element_located((By.ID, "senha")))
+
+        email_input.send_keys("ryan@gmail.com")
+        senha_input.send_keys("CD=hj0=r")
+        senha_input.send_keys(Keys.ENTER)
+
+        WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "balance")))
+        print(" Login realizado com sucesso! Home carregada.")
+
+        driver.get("http://localhost:8080/Depositar")
+        time.sleep(1)
+
+        valor_input = WebDriverWait(driver, 15).until(EC.visibility_of_element_located((By.ID, "valor")))
+        valor_input.clear()
+        valor_input.send_keys("11")
+
+        botao_submit = driver.find_element(By.CSS_SELECTOR, "#formDeposito button[type='submit']")
+        botao_submit.click()
+
+        mensagem_div = WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".message")))
+        mensagem_texto = mensagem_div.text
+
+        if "Erro: valor não pode ser ímpar" in mensagem_texto:
+            print("Teste de depósito 11 passou!")
+        else:
+            print("Teste falhou. Mensagem exibida:", mensagem_texto)
+
+        input("Pressione Enter para fechar o navegador...")
+
+    finally:
+        driver.quit()
+
+if __name__ == "__main__":
+    main()
---
+++ webApplication/selenium/deposito/test_exibe_30.py
@@ -0,0 +1,48 @@
+# test_valor_30.py
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+def main():
+    chrome_options = Options()
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--disable-notifications")
+    chrome_options.add_argument("--disable-infobars")
+
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+
+    try:
+        driver.get("http://localhost:8080/Login")
+        email_input = WebDriverWait(driver, 15).until(EC.visibility_of_element_located((By.ID, "email")))
+        senha_input = WebDriverWait(driver, 15).until(EC.visibility_of_element_located((By.ID, "senha")))
+        email_input.send_keys("ryan@gmail.com")
+        senha_input.send_keys("CD=hj0=r")
+        senha_input.submit()
+
+        WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "balance")))
+
+        driver.get("http://localhost:8080/Depositar")
+        valor_input = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "valor")))
+
+        valor_input.clear()
+        valor_input.send_keys("30")
+        valor_atual = valor_input.get_attribute("value")
+
+        if valor_atual == "30":
+            print("Valor 30 inserido corretamente!")
+        else:
+            print("Valor inserido incorreto:", valor_atual)
+
+        time.sleep(3)
+
+    finally:
+        driver.quit()
+
+if __name__ == "__main__":
+    main()
