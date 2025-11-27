3b213e634a7befbc933e06654aa5728b4c747323
Ryan Serra <ryanserrasilva@id.uff.br>
2025-11-27 10:28:46 -0300

    fix: renomeando nome da classe de investimentoTest

--- webApplication/src/test/java/com/mycompany/webapplication/usecases/InvestimentoTest.java
+++ webApplication/src/test/java/com/mycompany/webapplication/usecases/InvestimentoTest.java
@@ -5,7 +5,6 @@ import com.mycompany.webapplication.model.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -16,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class InvestimentoUCTest {
+class InvestimentoTest {
 
     @Mock
     AccountDAO accountDAO;
