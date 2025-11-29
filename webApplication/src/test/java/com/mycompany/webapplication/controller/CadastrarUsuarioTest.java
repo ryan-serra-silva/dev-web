f05b678733be8758a50e82a98a669d5186fb7ee4
Gabriel Menezes <106436496+gbmenezes97@users.noreply.github.com>
2025-11-29 19:56:32 +0000

    Correção de Classe

--- webApplication/src/test/java/com/mycompany/webapplication/controller/CadastrarUsuarioTest.java
+++ webApplication/src/test/java/com/mycompany/webapplication/controller/CadastrarUsuarioTest.java
@@ -20,19 +20,19 @@ public class CadastrarUsuarioTest {
 
     @Test
     public void testNomeVazio() {
-        String msg = CadastrarUsuarioUC.validarUsuario("", "teste@email.com", "Senha123");
+        String msg = CadastrarUsuarioUC.validarUsuario("", "teste@email.com", "Senha123", userDAO);
         assertEquals("Nome não pode estar vazio.", msg);
     }
 
     @Test
     public void testEmailVazio() {
-        String msg = CadastrarUsuarioUC.validarUsuario("Gabriel", "", "Senha123");
+        String msg = CadastrarUsuarioUC.validarUsuario("Gabriel", "", "Senha123", userDAO);
         assertEquals("E-mail não pode estar vazio.", msg);
     }
 
     @Test
     public void testEmailInvalidoSemArroba() {
-        String msg = CadastrarUsuarioUC.validarUsuariolidacao("Gabriel", "testeemail.com", "Senha123");
+        String msg = CadastrarUsuarioUC.validarUsuariolidacao("Gabriel", "testeemail.com", "Senha123", userDAO);
         assertEquals("E-mail inválido. Deve conter '@' e '.'", msg);
     }
 
@@ -40,50 +40,49 @@ public class CadastrarUsuarioTest {
     public void testEmailJaExistente() {
         Users u = MockGenerator.createUser();
         Mockito.when(userDAO.getByEmail("existente@email.com")).thenReturn(u);
-        String msg = CadastrarUsuarioUC.validarUsuariolidacao("João", "existente@email.com", "Senha123");
+        String msg = CadastrarUsuarioUC.validarUsuariolidacao("João", "existente@email.com", "Senha123", userDAO);
         assertEquals("E-mail já está em uso. Tente outro.", msg);
     }
 
     @Test
     public void testSenhaVazia() {
-        String msg = CadastrarUsuarioUC.validarUsuario("Maria", "maria@email.com", "");
+        String msg = CadastrarUsuarioUC.validarUsuario("Maria", "maria@email.com", "", userDAO), ;
         assertEquals("Senha não pode estar vazia.", msg);
     }
 
     @Test
     public void testSenhaCurta() {
-        String msg = CadastrarUsuarioUC.validarUsuario("Maria", "maria@email.com", "Ab1");
+        String msg = CadastrarUsuarioUC.validarUsuario("Maria", "maria@email.com", "Ab1", userDAO);
         assertEquals("Senha deve ter pelo menos 6 caracteres.", msg);
     }
 
     @Test
     public void testSenhaSemNumeroNemEspecial() {
-        String msg = CadastrarUsuarioUC.validarUsuario("Carlos", "carlos@email.com", "Senhaa");
+        String msg = CadastrarUsuarioUC.validarUsuario("Carlos", "carlos@email.com", "Senhaa", userDAO);
         assertEquals("Senha deve conter pelo menos um número ou caractere especial.", msg);
     }
 
     @Test
     public void testSenhaSemMaiusculaEMinuscula() {
-        String msg = CadastrarUsuarioUC.validarUsuario("Lucas", "lucas@email.com", "1234567");
+        String msg = CadastrarUsuarioUC.validarUsuario("Lucas", "lucas@email.com", "1234567", userDAO);
         assertEquals("Senha deve conter pelo menos um número ou caractere especial.", msg);
     }
 
     @Test
     public void testSenhaContemNome() {
-        String msg = CadastrarUsuarioUC.validarUsuariolidacao("Lucas", "lucas@email.com", "Lucas12#3");
+        String msg = CadastrarUsuarioUC.validarUsuariolidacao("Lucas", "lucas@email.com", "Lucas12#3", userDAO);
         assertEquals("Senha não pode conter o nome ou o e-mail.", msg);
     }
 
     @Test
     public void testSenhaComEspaco() {
-        String msg = CadastrarUsuarioUC.validarUsuario("Pedro", "pedro@email.com", "Senha 12#3");
+        String msg = CadastrarUsuarioUC.validarUsuario("Pedro", "pedro@email.com", "Senha 12#3", userDAO);
         assertEquals("Senha não pode conter espaços.", msg);
     }
 
     @Test
     public void testSenhaValida() {
-        String msg = CadastrarUsuarioUC.validarUsuario("Pedro", "pedro@email.com", "Senha123!");
+        String msg = CadastrarUsuarioUC.validarUsuario("Pedro", "pedro@email.com", "Senha123!", userDAO);
         assertEquals("Senha não pode começar ou terminar com caractere especial.",msg);
     }
-
-    
+}
