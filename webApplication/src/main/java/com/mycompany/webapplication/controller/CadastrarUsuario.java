449a1501084e4b634b77539cb1e26dcd41b715e5
Gabriel Menezes <106436496+gbmenezes97@users.noreply.github.com>
2025-11-29 17:49:10 +0000

    Criação da Classe Uc + Ajuste de Classe

--- webApplication/src/main/java/com/mycompany/webapplication/controller/CadastrarUsuario.java
+++ webApplication/src/main/java/com/mycompany/webapplication/controller/CadastrarUsuario.java
@@ -72,47 +72,6 @@ public class CadastrarUsuario extends HttpServlet {
                 .forward(request, response);
     }
 
-    private String validarUsuario(String nome, String email, String senha, UserDAO userDAO) {
-        if (nome == null || nome.trim().isEmpty()) {
-            return "Nome não pode estar vazio.";
-        } else if (email == null || email.trim().isEmpty()) {
-            return "E-mail não pode estar vazio.";
-        } else if (!email.contains("@") || !email.contains(".")) {
-            return "E-mail inválido. Deve conter '@' e '.'";
-        } else {
-            Users existente = userDAO.getByEmail(email);
-            if (existente != null) {
-                return "E-mail já está em uso. Tente outro.";
-            }
-            if (senha == null || senha.isEmpty()) {
-                return "Senha não pode estar vazia.";
-            }
-            if (senha.length() < 6) {
-                return "Senha deve ter pelo menos 6 caracteres.";
-            }
-            if (!senha.matches(".*\\d.*") || !senha.matches(".*[!@#$%^&()].*")) {
-                return "Senha deve conter pelo menos um número ou caractere especial.";
-            }
-            if (!senha.matches(".*[A-Z].*") || !senha.matches(".*[a-z].*")) {
-                return "Senha deve conter letras maiúsculas e minúsculas.";
-            }
-            if (senha.toLowerCase().contains(nome.toLowerCase()) || senha.equalsIgnoreCase(email)) {
-                return "Senha não pode conter o nome ou o e-mail.";
-            }
-            if (senha.matches("^(123456|abcdef|senha|password)$")) {
-                return "Senha muito fraca. Escolha outra.";
-            }
-            if (senha.matches("^[!@#$%^&*()].*") || senha.matches(".*[!@#$%^&*()]$")) {
-                return "Senha não pode começar ou terminar com caractere especial.";
-            }
-
-            if (senha.contains(" ")) {
-                return "Senha não pode conter espaços.";
-            }
-            return null;
-        }
-    }
-
     private String gerarNumeroContaAleatorio() {
         int numero = (int) (Math.random() * 900000) + 100000;
         return String.valueOf(numero);
